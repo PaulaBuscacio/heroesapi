67a4f6cd0efdb8bdbea19f4f7e4ac1b7f0bc6c0d
PaulaBuscacio <paula@buscacio.net>
2021-03-17 17:49:48 -0300

    projeto-livecoding

---
+++ README.md
@@ -0,0 +1,1 @@
+## API de heróis contruída em conjunto com live coding da Digital Innovation One
---
+++ src/main/java/com/digitalinnovationone/heroesapi/constans/HeroesConstant.java
@@ -0,0 +1,12 @@
+package com.digitalinnovationone.heroesapi.constans;
+
+import com.amazonaws.services.dynamodbv2.xspec.S;
+
+public class HeroesConstant {
+
+    public static final String HEROES_ENDPOINT_LOCAL = "/heroes";
+    public static final String ENDPOINT_DYNAMO = "Https://localhost:8060";
+    public static final String REGION_DYNAMO = "us-east-1";
+
+
+}
